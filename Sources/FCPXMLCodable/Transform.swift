ddcaaf998877118af998e86bd70719d04cfecdba
Jack Youstra <jack@youstra.com>
2024-03-20 10:48:55 -0700

    Make transform parameters optional

--- Sources/FCPXMLCodable/Transform.swift
+++ Sources/FCPXMLCodable/Transform.swift
@@ -18,26 +18,26 @@ public struct Transform: XMLRepresentable {
     public var parameters: [Parameter] = []
     
     /// A Boolean value indicating whether the transform adjustment is enabled.
-    public var isEnabled: Bool = true
+    public var isEnabled: Bool?
     
     /// The position of the transform adjustment.
-    public var position: Point
+    public var position: Point?
     
     /// The scale of the transform adjustment.
-    public var scale: Point
+    public var scale: Point?
     
     /// The rotation of the transform adjustment.
-    public var rotation: Double
+    public var rotation: Double?
     
     /// The anchor point of the transform adjustment.
-    public var anchor: Point
+    public var anchor: Point?
     
     /// Initializes a new transform adjustment.
     /// - Parameter position: The position of the transform adjustment.
     /// - Parameter scale: The scale of the transform adjustment.
     /// - Parameter rotation: The rotation of the transform adjustment.
     /// - Parameter anchor: The anchor point of the transform adjustment.
-    public init(position: Point, scale: Point, rotation: Double, anchor: Point) {
+    public init(position: Point? = nil, scale: Point? = nil, rotation: Double? = nil, anchor: Point? = nil) {
         self.position = position
         self.scale = scale
         self.rotation = rotation
@@ -46,11 +46,11 @@ public struct Transform: XMLRepresentable {
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        isEnabled = try container.decodeIfPresent(Bool.self, forKey: .isEnabled) ?? true
-        position = try container.decodeIfPresent(Point.self, forKey: .position) ?? .zero
-        scale = try container.decodeIfPresent(Point.self, forKey: .scale) ?? Point(x: 1, y: 1)
-        rotation = try container.decodeIfPresent(Double.self, forKey: .rotation) ?? 0
-        anchor = try container.decodeIfPresent(Point.self, forKey: .anchor) ?? .zero
+        isEnabled = try container.decodeIfPresent(Bool.self, forKey: .isEnabled)
+        position = try container.decodeIfPresent(Point.self, forKey: .position)
+        scale = try container.decodeIfPresent(Point.self, forKey: .scale)
+        rotation = try container.decodeIfPresent(Double.self, forKey: .rotation)
+        anchor = try container.decodeIfPresent(Point.self, forKey: .anchor)
         parameters = try container.decode([Parameter].self, forKey: .parameters)
     }
     
